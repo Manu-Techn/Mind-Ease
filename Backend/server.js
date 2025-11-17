791d4938798b637288824de9317b59506a548006
Emmanuel <emmanuelochieng35@gmail.com>
2025-11-17 15:56:05 +0300

    Update

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use('/api/auth', require('./routes/auth'));
+app.use('/api/auth', require('./Routes/auth'));
 app.use('/api/gratitude', require('./Routes/gratitude'));
 
 // Basic route
